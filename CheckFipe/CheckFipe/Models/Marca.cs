ba4db478745ff5e0000748e961fce41b96938b70
Ana Santos <anasantos1103@gmail.com>
2019-09-28 17:09:19 -0300

    Criado solução e projeto com controller inicial da entidade marca.

---
+++ CheckFipe/CheckFipe/Models/Marca.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace CheckFipe.Models
+{
+    public class Marca
+    {
+        [JsonProperty("id")]
+        public long CodigoMarca { get; set; }
+
+        [JsonProperty("fipe_name")]
+        public string NomeMarca { get; set; }
+    }
+}
