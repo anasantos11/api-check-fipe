b799815dc3029a64dc6d2a35d8c3b8cc5b271ed8
Ana Santos <anasantos1103@gmail.com>
2019-09-28 19:12:57 -0300

    Criado enums TipoVeiculoFipe e AcaoFipe para auxiliar na construção da url da api da Fipe.

---
+++ CheckFipe/CheckFipe/Enums/AcaoFipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckFipe.Enums
+{
+    public enum AcaoFipe
+    {
+        Marcas,
+        Veiculos,
+        Veiculo
+    }
+}
