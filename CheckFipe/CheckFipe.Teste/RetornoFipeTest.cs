84a8f6a6940649d79607bd073462f2e36368ebad
Ana Santos <anasantos1103@gmail.com>
2019-09-28 19:55:09 -0300

    Criado classe com estrutura de retorno dos serviços da fipe.

---
+++ CheckFipe/CheckFipe.Teste/RetornoFipeTest.cs
@@ -0,0 +1,29 @@
+﻿using CheckFipe.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckFipe.Teste
+{
+    public class RetornoFipeTest
+    {
+        [Test]
+        public void ValidarSerializacaoObjeto()
+        {
+            var retornoFipe = new RetornoFipe()
+            {
+                Codigo = 21,
+                Nome = "Fiat"
+            };
+
+            string retornoFipeJson = JsonConvert.SerializeObject(retornoFipe);
+            JObject retornoFipeJbject = JObject.Parse(retornoFipeJson);
+
+            Assert.AreEqual(21, retornoFipeJbject["id"].Value<long>());
+            Assert.AreEqual("Fiat", retornoFipeJbject["fipe_name"].Value<string>());
+        }
+    }
+}
---
+++ CheckFipe/CheckFipe/Models/RetornoFipe.cs
@@ -0,0 +1,18 @@
+﻿using CheckFipe.Enums;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckFipe.Models
+{
+    public class RetornoFipe
+    {
+        [JsonProperty("id")]
+        public long Codigo { get; set; }
+
+        [JsonProperty("fipe_name")]
+        public virtual string Nome { get; set; }
+    }
+}
