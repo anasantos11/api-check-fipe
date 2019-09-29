2f2102608906531e079154a0c112f51c2a0afc8a
Ana Santos <anasantos1103@gmail.com>
2019-09-28 22:00:35 -0300

    Adicionado swagger.

--- CheckFipe/CheckFipe/Controllers/ConsultaMarcaController.cs
+++ CheckFipe/CheckFipe/Controllers/ConsultaMarcaController.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using CheckFipe.Enums;
 using CheckFipe.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Converters;
 
 namespace CheckFipe.Controllers
 {
+    /// <summary> Consulta das marcas dos veículo</summary>
+    /// <remarks> Exemplo requisição:    GET /api/ConsultaMarca/0 </remarks>
+    /// <param name="tipoVeiculo">Tipo do Veículo</param>
+    /// <returns>Lista contendo todas as marcas do tipo de veículo informado, com o código e nome da marca.</returns>
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class ConsultaMarcaController : ControllerBase
@@ -16,7 +23,7 @@ namespace CheckFipe.Controllers
         [HttpGet("{tipoVeiculo}")]
         public IEnumerable<RetornoFipe> Get(TipoVeiculoFipe tipoVeiculo)
         {
-           return new ConsultaMarca(tipoVeiculo).Carregar();
+            return new ConsultaMarca(tipoVeiculo).Carregar();
         }
     }
 }
--- CheckFipe/CheckFipe/Startup.cs
+++ CheckFipe/CheckFipe/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CheckFipe
 {
@@ -25,6 +28,26 @@ namespace CheckFipe
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(configSwagger =>
+            {
+                configSwagger.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Check Fipe",
+                        Version = "v1",
+                        Description = "Serviços para consulta de marcas, modelos, versões e preço de um veículo na tabela fipe.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Ana Paula dos Santos",
+                            Url = new Uri("https://github.com/anasantos11")
+                        }
+                    });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                configSwagger.IncludeXmlComments(xmlPath);
+                configSwagger.DescribeAllEnumsAsStrings();
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -43,6 +66,12 @@ namespace CheckFipe
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(configSwagger =>
+            {
+                configSwagger.SwaggerEndpoint("/swagger/v1/swagger.json", "Check Fipe v1");
             });
         }
     }
