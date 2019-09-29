a59d47248f58e8b9b5ccf1642c07294d48e02fbb
Ana Santos <anasantos1103@gmail.com>
2019-09-28 22:36:12 -0300

    Criado consulta do modelo.

---
+++ CheckFipe/CheckFipe/Controllers/ConsultaModeloController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using CheckFipe.Enums;
+using CheckFipe.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Converters;
+
+namespace CheckFipe.Controllers
+{
+    /// <summary> Consulta os modelos de uma marca de veículo</summary>
+    /// <remarks> Exemplo requisição:    GET /api/ConsultaModelo/Carros/21 </remarks>
+    /// <param name="tipoVeiculo">Tipo do Veículo</param>
+    /// <param name="codigoMarca">Código da Marca</param>
+    /// <returns>Lista contendo todos os modelos da marca, com o código e nome do modelo.</returns>
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultaModeloController : ControllerBase
+    {
+        [HttpGet("{tipoVeiculo}/{codigoMarca}")]
+        public IEnumerable<Marca> Get(TipoVeiculoFipe tipoVeiculo, int codigoMarca)
+        {
+            return new ConsultaFipe(tipoVeiculo, AcaoFipe.Veiculos, codigoMarca.ToString()).Carregar();
+        }
+    }
+}
