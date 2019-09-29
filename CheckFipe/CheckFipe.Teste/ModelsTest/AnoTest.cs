9252a978e810a98eb2a3fbb3013d4a17e3a874ab
Ana Santos <anasantos1103@gmail.com>
2019-09-29 13:56:13 -0300

    Criado serviço para consulta do ano.

---
+++ CheckFipe/CheckFipe.Teste/ModelsTest/AnoTest.cs
@@ -0,0 +1,24 @@
+﻿using CheckFipe.Enums;
+using CheckFipe.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckFipe.Teste.ModelsTest
+{
+    public class AnoTest
+    {
+        [TestCase(TipoVeiculoFipe.Carros, 21, 4828, "2013-1", "2013 Gasolina")]
+        [TestCase(TipoVeiculoFipe.Motos, 101, 3060, "1995-1", "1995")]
+        [TestCase(TipoVeiculoFipe.Caminhoes, 109, 3302, "1997-3", "1997")]
+        public void ValidarCarregamentoDosAnos(TipoVeiculoFipe tipoVeiculo, long codigoMarca, long codigoModelo, string codigoAnoEsperado, string descricaoAnoEsperado)
+        {
+
+            IEnumerable<Ano> retorno = Ano.Carregar(tipoVeiculo, codigoMarca, codigoModelo);
+            Assert.IsNotNull(retorno);
+            Assert.IsTrue(retorno.Count(marca => marca.Codigo == codigoAnoEsperado && marca.Nome == descricaoAnoEsperado) > 0);
+        }
+    }
+}
---
+++ CheckFipe/CheckFipe/Models/Ano.cs
@@ -0,0 +1,25 @@
+﻿using CheckFipe.Enums;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckFipe.Models
+{
+    public class Ano
+    {
+        public string Codigo { get; set; }
+
+        public string Nome { get; set; }
+
+        public static IEnumerable<Ano> Carregar(TipoVeiculoFipe tipoVeiculo, long codigoMarca, long codigoModelo)
+        {
+            return new ConsultaFipe(tipoVeiculo, AcaoFipe.Veiculo, codigoMarca.ToString(), codigoModelo.ToString())
+                .Carregar()
+                .Select(retornoFipe => new Ano()
+                {
+                    Codigo = retornoFipe.Codigo,
+                    Nome = retornoFipe.Nome
+                });
+        }
+    }
+}
