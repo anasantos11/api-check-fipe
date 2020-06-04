87e720d9c74a933eb48f7d7f79aa64a65d1a4c6f
Ana Santos <anasantos1103@gmail.com>
2020-06-04 17:40:06 -0300

    Criado serviço do modelo para carregar dados da Fipe.

---
+++ CheckFipe/CheckFipe.Domain/ServicesInterfaces/IModeloService.cs
@@ -0,0 +1,10 @@
+﻿using CheckFipe.Domain.Entities;
+using System.Collections.Generic;
+
+namespace CheckFipe.Domain.ServicesInterfaces
+{
+    public interface IModeloService
+    {
+        IEnumerable<Modelo> Carregar();
+    }
+}
---
+++ CheckFipe/CheckFipe.Infraestructure.Proxy/Services/ModeloService.cs
@@ -0,0 +1,29 @@
+﻿using CheckFipe.Domain.Entities;
+using CheckFipe.Domain.Enumerators;
+using CheckFipe.Domain.ServicesInterfaces;
+using CheckFipe.Infraestructure.Proxy.DataTransferObjects;
+using CheckFipe.Infraestructure.Proxy.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckFipe.Infraestructure.Proxy.Services
+{
+    public class ModeloService : FipeBaseService, IModeloService
+    {
+        public ModeloService(TipoVeiculo tipoVeiculo, string codigoMarca): base(tipoVeiculo, TipoAcaoFipe.Veiculos, codigoMarca)
+        {
+
+        }
+        public IEnumerable<Modelo> Carregar()
+        {
+            return Carregar<IEnumerable<FipeBaseOutput>>()
+                .Select(retornoFipe => new Modelo()
+                {
+                    Id = Convert.ToInt64(retornoFipe.Codigo),
+                    Nome = retornoFipe.Nome
+                })
+                .OrderBy(retornoFipe => retornoFipe.Nome);
+        }
+    }
+}
