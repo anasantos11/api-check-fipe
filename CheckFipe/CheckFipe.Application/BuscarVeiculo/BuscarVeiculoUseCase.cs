d99569a827de14f6e81ee4314888171304d058fb
Ana Santos <anasantos1103@gmail.com>
2020-06-04 23:09:11 -0300

    Criado caso de uso de buscar veículo no projeto Application.

---
+++ CheckFipe/CheckFipe.Application/BuscarVeiculo/BuscarVeiculoUseCase.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CheckFipe.Domain.Enumerators;
+using CheckFipe.Domain.ServicesInterfaces;
+
+namespace CheckFipe.Application.BuscarVeiculo
+{
+    public class BuscarVeiculoUseCase : IBuscarVeiculoUseCase
+    {
+        private readonly IVeiculoService VeiculoService;
+        private readonly IMapper Mapper;
+        public BuscarVeiculoUseCase(IVeiculoService veiculoService, IMapper mapper)
+        {
+            this.VeiculoService = veiculoService;
+            this.Mapper = mapper;
+        }
+
+        public VeiculoOutput Carregar(TipoVeiculo tipoVeiculo, long codigoMarca, long codigoModelo, string codigoAno)
+        {
+            return this.Mapper.Map<VeiculoOutput>(this.VeiculoService.Carregar(tipoVeiculo, codigoMarca, codigoModelo, codigoAno));
+        }
+    }
+}
---
+++ CheckFipe/CheckFipe.Application/BuscarVeiculo/IBuscarVeiculoUseCase.cs
@@ -0,0 +1,9 @@
+﻿using CheckFipe.Domain.Enumerators;
+
+namespace CheckFipe.Application.BuscarVeiculo
+{
+    public interface IBuscarVeiculoUseCase
+    {
+        VeiculoOutput Carregar(TipoVeiculo tipoVeiculo, long codigoMarca, long codigoModelo, string codigoAno);
+    }
+}
